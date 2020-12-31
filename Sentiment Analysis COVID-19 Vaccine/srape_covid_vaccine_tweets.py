4594d6495ed7ce906a214d438ca322ec488e4676
Fadina Adeolu <fadinaadeolu@gmail.com>
2020-12-31 15:13:37 +0100

    update scrape covid vaccine tweets

--- Sentiment Analysis COVID-19 Vaccine/srape_covid_vaccine_tweets.py
+++ Sentiment Analysis COVID-19 Vaccine/srape_covid_vaccine_tweets.py
@@ -1,11 +1,15 @@
 import snscrape.modules.twitter as sntwitter
 import pandas as pd
+import datetime
+#start = input("Enter start date (YYYY-MM-DD): ")
 
-start = input("Enter start date (YYYY-MM-DD): ")
-def scrape_twitter(start):
-    
+def scrape_twitter(start, stop):
+
     tweets_list = []
-    for tweet in sntwitter.TwitterSearchScraper("(covid-19 vaccine) OR #vaccine since:{}".format(start)).get_items():
+    for tweet in sntwitter.TwitterSearchScraper("""(covid-19 vaccine)
+        OR #vaccine
+        OR #Coronavirusvaccine
+        OR #COVID19Vaccine since:{} until:{}""".format(start, stop)).get_items():
         tweets_list.append([tweet.id,
                             tweet.date,
                             tweet.content,
@@ -30,10 +34,21 @@ def scrape_twitter(start):
                             ])
     return tweets_df
 
-tweets_df = scrape_twitter(start)
-tweets_df.to_csv("./data/tweets_covid_19_{}.csv".format(start), index=False)
+d = pd.date_range("2020-11-1", "2020-12-30", freq="D")
+dt = [i.strftime("%Y-%m-%d") for i in d]
 
-print(tweets_df.head(1))
+for i in range(0, len(dt)+1):
+    try:
+        start = dt[i]
+        stop = dt[i+1]
+    except IndexError:
+        start = dt[-1]
+        stop = datetime.datetime.now().strftime("%Y-%m-%d")
+
+    tweets_df = scrape_twitter(start, stop)
+    tweets_df.to_csv("./data/tweets_covid_2020-11-1.csv".format(start), mode="a", header=False, index=False)
+
+    print(stop)
 
 # Reference
 # https://medium.com/better-programming/how-to-scrape-tweets-with-snscrape-90124ed006af
